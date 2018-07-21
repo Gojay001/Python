98a0eca7b479d604c982afaf16a7ebe6871beb7f
gojay <939926392@qq.com>
2018-07-21 17:20:11 +0800

    seventh day

---
+++ get_jdinfo.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# get_jdinfo.py
+
+from bs4 import BeautifulSoup
+import re,requests,json
+
--- urllib_test.py
+++ urllib_test.py
@@ -6,14 +6,14 @@
 # Get
 from urllib import request
 ## urllib的request模块发送get请求:
-def getinfo(url):
+def printinfo(url):
 	with request.urlopen(url) as f:
 		print('Status:', f.status, f.reason)
 		for k, v in f.getheaders():
 			print('%s: %s' % (k, v))
 		data = f.read()
 		print('Data:', data.decode('utf-8'))
-#getinfo('https://api.douban.com/v2/book/2129650')
+#printinfo('https://api.douban.com/v2/book/2129650')
 
 ## 使用Request对象把请求伪装成浏览器:
 def imitate(url):
@@ -27,11 +27,26 @@ def imitate(url):
 	print('Data:', data.decode('utf-8'))
 #imitate('http://www.douban.com/')
 
-def printinfo(url):
+## 发送get请求
+def getinfo(url):
 	from urllib.request import urlopen
 	html = urlopen(url).read().decode('utf-8')
-	print(html)
-printinfo('https://morvanzhou.github.io/static/scraping/basic-structure.html')
+	return html
+
+html = getinfo('https://morvanzhou.github.io/static/scraping/basic-structure.html')
+# print(html)
+
+## 使用正则表达式匹配信息
+import re
+### 获取<title>
+res1 = re.findall(r'<title>(.+?)</title>', html)
+### 获取<p>: flags=re.DOTALL 来对 tab, new line 不敏感
+res2 = re.findall(r'<p>(.*?)</p>', html, flags=re.DOTALL)
+### 获取所有链接
+res3 = re.findall(r'href="(.*?)"', html)
+print('\nPage title is: ', res1[0])
+print('\nPage paragraph is:	 ', res2[0])
+print('\nAll links: ', res3)
 
 # Post
 ## 如果要以POST发送一个请求，只需要把参数data以bytes形式传入

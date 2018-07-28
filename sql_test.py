d7e839605a61d5194018bd0a790c62f69e55a64b
gojay <939926392@qq.com>
2018-07-28 09:46:42 +0800

    update

--- sql_test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-
-import pymysql
-import csv
-import codecs
--- turtle_test.py
+++
@@ -1,5 +0,0 @@
-import turtle
-t = turtle.Pen()
-for x in range(0,4):
-	t.forward(100)
-	t.left(90)
